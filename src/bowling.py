9ee68c3039e953f5579adbce44bbb33c505394f5
hsaltinsoy <h.saidaltinsoy@gmail.com>
2024-02-17 15:52:25 +0000

    added test for score

--- src/bowling.py
+++ src/bowling.py
@@ -4,17 +4,26 @@ from numpy.random import randint
 class Game:
     first_roll: int
     second_roll: int
+    frame = 0
 
-    def first_roll_func(self):
+    def first_roll_func(self) -> int:
         self.first_roll = randint(0, 10)
         if self.first_roll == 10:
+            self.frame += 1
             return self.first_roll
         else:
             return self.second_roll_func()
 
-    def second_roll_func(self):
+    def second_roll_func(self) -> int:
         self.second_roll = randint(0, 10 - self.first_roll)
         if self.first_roll + self.second_roll == 10:
+            self.frame += 1
             return 10
         else:
+            self.frame += 1
             return self.first_roll + self.second_roll
+
+    def score(self) -> int:
+        if self.first_roll != 10 and self.first_roll + self.second_roll != 10:
+            return self.first_roll + self.second_roll
+
--- tests/test_bowling.py
+++ tests/test_bowling.py
@@ -4,23 +4,29 @@ from unittest.mock import patch
 
 
 class TestGameRollMethod(unittest.TestCase):
+    game = Game()
+
     @patch('src.bowling.randint', side_effect=[10])
     def test_strike_roll_successful(self, mock_randint):
-        game = Game()
-        result = game.first_roll_func()
+        result = self.game.first_roll_func()
         self.assertEqual(result, 10)
         mock_randint.assert_called_with(0, 10)
 
     @patch('src.bowling.randint', side_effect=[5, 4])
     def test_normal_roll_successful(self, mock_randint):
-        game = Game()
-        result = game.first_roll_func()
+        result = self.game.first_roll_func()
         self.assertEqual(result, 9)
         mock_randint.assert_called_with(0, 10 - 5)
 
     @patch('src.bowling.randint', side_effect=[5, 5])
     def test_spare_roll_successful(self, mock_randint):
-        game = Game()
-        result = game.first_roll_func()
+        result = self.game.first_roll_func()
         self.assertEqual(result, 10)
+        mock_randint.assert_called_with(0, 10 - 5)
+
+    @patch('src.bowling.randint', side_effect=[5, 4])
+    def test_score_roll_successful(self, mock_randint):
+        self.game.first_roll_func()
+        score = self.game.score()
+        self.assertEqual(score, 9)
         mock_randint.assert_called_with(0, 10 - 5)
