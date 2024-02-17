9c6f49c74c8b8ab636d85143234966fb5bc084e7
hsaltinsoy <h.saidaltinsoy@gmail.com>
2024-02-17 15:24:52 +0000

    added test for spare roll

--- tests/test_bowling.py
+++ tests/test_bowling.py
@@ -5,14 +5,22 @@ from unittest.mock import patch
 
 class TestGameRollMethod(unittest.TestCase):
     @patch('src.bowling.randint', side_effect=[10])
-    def test_first_roll_successful(self, mock_randint):
+    def test_strike_roll_successful(self, mock_randint):
         game = Game()
         result = game.first_roll_func()
         self.assertEqual(result, 10)
+        mock_randint.assert_called_with(0, 10)
 
     @patch('src.bowling.randint', side_effect=[5, 4])
-    def test_second_roll_successful(self, mock_randint):
+    def test_normal_roll_successful(self, mock_randint):
         game = Game()
         result = game.first_roll_func()
         self.assertEqual(result, 9)
+        mock_randint.assert_called_with(0, 10 - 5)
+
+    @patch('src.bowling.randint', side_effect=[5, 5])
+    def test_spare_roll_successful(self, mock_randint):
+        game = Game()
+        result = game.first_roll_func()
+        self.assertEqual(result, 10)
         mock_randint.assert_called_with(0, 10 - 5)
